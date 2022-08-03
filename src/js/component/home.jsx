9aa198f0bc4560370dc7d7fb130c5fa2c31b0e1e
stevencarle93 <stevencarle@hotmail.com>
2022-08-03 03:33:00 +0000

    Code Done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,32 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export default function Home(props) {
 
-//create your first component
-const Home = () => {
+	const counterStyle = {
+		color: "white", 
+		fontSize: "8rem", 
+		padding: "1rem"
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col">
+					<div className="d-flex justify-content-center bg-dark" style={{ height: "15rem" }}>
+						<div className="text-justify" style={{ color: "white", fontSize: "8rem" }}>
+							<FontAwesomeIcon icon={faClock} />
+						</div>
+						<div className = "text-justify" style={ counterStyle }> {props.digitSix} </div>
+						<div className = "text-justify" style={ counterStyle }> {props.digitFive} </div>
+						<div className = "text-justify" style={ counterStyle }> {props.digitFour} </div>
+						<div className = "text-justify" style={ counterStyle }> {props.digitThree} </div>
+						<div className = "text-justify" style={ counterStyle }> {props.digitTwo} </div>
+						<div className = "text-justify" style={ counterStyle }> {props.digitOne} </div>
+					</div>
+				</div>
+			</div>
 		</div>
-	);
+	)
 };
-
-export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,59 @@
-//import react into the bundle
+import { counter } from "@fortawesome/fontawesome-svg-core";
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counterOne = 0
+let counterTwo = 0
+let counterThree = 0
+let counterFour = 0
+let counterFive = 0
+let counterSix = 0
+
+function countValidation() {
+    if (counterOne < 9) counterOne++
+    else if (counterTwo < 9){
+        counterOne = 0
+        counterTwo++
+    }
+    else if (counterThree < 9){
+        counterOne = 0
+        counterTwo = 0
+        counterThree++
+    }
+    else if (counterFour < 9){
+        counterOne = 0
+        counterTwo = 0
+        counterThree = 0
+        counterFour++
+    }
+    else if (counterFive < 9){
+        counterOne = 0
+        counterTwo = 0
+        counterThree = 0
+        counterFour = 0
+        counterFive++
+    }
+    else if (counterSix < 9){
+        counterOne = 0
+        counterTwo = 0
+        counterThree = 0
+        counterFour = 0
+        counterFive = 0
+        counterSix++
+    }
+}
+
+setInterval(() => {
+    
+    countValidation()
+
+    ReactDOM.render(<Home 
+        digitOne = {counterOne}
+        digitTwo = {counterTwo}
+        digitThree = {counterThree}
+        digitFour = {counterFour}
+        digitFive = {counterFive}
+        digitSix = {counterSix}/>, document.querySelector("#app"))
+}, 1000);
